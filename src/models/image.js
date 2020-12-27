235600708e5aecb459951c719db3c6aee2e4f7f9
Kaan Yilmaz <kaan.yilmaz@ormuco.com>
2020-12-27 16:23:54 -0700

    apply prettier

--- src/models/image.js
+++ src/models/image.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    image:{
-        data: Buffer,
-        contentType: String,
-        filename: String
-    },
-    created: {
-        type: Date, 
-        default: Date.now
-    }
+  title: String,
+  description: String,
+  image: {
+    data: Buffer,
+    contentType: String,
+    filename: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Image = mongoose.model("Image", imageSchema);
